a50baea1f733b98b0e28a5e4712d4a6ba2469149
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-02-28 13:49:58 +0100

    update 0.5.3

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function start() {
 
 startAnimation()
 startPosition()
+startPositionPotion()
 registerClick()
 
 document.querySelector("#mob1_container")
@@ -33,6 +34,10 @@ function startPosition() {
   document.querySelector("#human_container").classList.add("position4")
 }
 
+function startPositionPotion() {
+  document.querySelector("#potion_container").classList.add("potion_position1")
+}
+
 function registerClick() {
 document.querySelector("#mob1_container").addEventListener("click", mobClicked);
 document.querySelector("#mob2_container").addEventListener("click", mobClicked);
@@ -40,37 +45,74 @@ document.querySelector("#mob3_container").addEventListener("click", mobClicked);
 document.querySelector("#human_container").addEventListener("click", humanClicked);
 }
 
+//JS for enemies
 function mobClicked() {
   console.log("Mob clicked")
+
   let mob = this;
   mob.removeEventListener("click", mobClicked)
   mob.classList.add("paused")
   mob.querySelector("img").classList.add("zoom_out_spin")
-  mob.addEventListener("animationEnd", mobGone)
+  mob.addEventListener("animationend", mobGone);
 }
-
 function mobGone() {
+
   let mob = this;
-  mob.removeEventListener("animationEnd", mobGone)
+
+  mob.removeEventListener("animationend", mobGone);
   mob.querySelector("img").classList.remove("zoom_out_spin")
   mob.classList.remove("paused")
-  mobRestart.call(this)
+  mobRestart.call(this);
   mob.addEventListener("click", mobClicked)
 }
+function mobRestart(){ 
 
-function mobRestart() {
   let mob = this;
-  mob.classList.remove("run")
-  mob.offsetwidth;
-  mob.classList.add("run")
+
+  mob.classList.remove("run");
+  mob.offsetWidth;
+  mob.classList.add("run");
   mob.classList
-  .remove("position1", "position2", "position3", "position4")
+  .remove("position1", "position2", "position3", "position4");
 
   let pos = Math.floor(Math.random()*4)+1;
   mob.classList.add("position" + pos)
-
 }
 
+//JS for villager
 function humanClicked() {
   console.log("Human clicked");
+
+  let human = this;
+  human.removeEventListener("click", humanClicked)
+  human.classList.add("paused")
+  human.querySelector("img").classList.add("fly_heaven")
+  human.addEventListener("animationend", humanGone)
+}
+function humanGone() {
+  let human = this;
+
+  human.removeEventListener("animationend", humanGone)
+  human.querySelector("img").classList.remove("fly_heaven")
+  humanRestart.call(this)
+  human.addEventListener("click", humanClicked)
+}
+function humanRestart() { 
+
+  let human = this;
+
+  human.classList.remove("run");
+  human.offsetWidth;
+  human.classList.add("run");
+  human.classList
+  .remove("position1", "position2", "position3", "position4");
+  human.classList.remove("paused")
+
+  let pos = Math.floor(Math.random()*4)+1;
+  human.classList.add("position" + pos)
+}
+
+//JS for health potion
+function potionClicked() {
+
 }
